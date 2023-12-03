b435219c21b6d0309febe0910256b5b26a56f119
pnguyen121 <phillynguyen17@gmail.com>
2023-12-03 18:22:38 -0500

    updates to home page, image, btn, text

--- Phil-ATM-Website/src/Components/Header/Header.jsx
+++ Phil-ATM-Website/src/Components/Header/Header.jsx
@@ -1,12 +1,50 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav class="bg-zinc-800 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-zinc-800">
-      <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <Link to={"/"} class="flex items-center text-white">
-          <img src="https://imgur.com/ViCzhzJ.png" id="logo" alt="" />
+    <nav className="bg-zinc-800 border-gray-200 px-3 sm:px-6 py-2.5 dark:bg-zinc-800">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <Link to={"/"} className="flex items-center text-white">
+          <h3>PHIL - ATM</h3>
         </Link>
+
+        {/* Hamburger menu for mobile */}
+        <div className="lg:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none focus:text-white"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+
+        {/* Nav links */}
+        <div className={`lg:flex ${menuOpen ? 'flex' : 'hidden'}`}>
+          {/* Add your navigation links here */}
+          <Link to={"/"} className="text-white px-4 py-2 hover:bg-gray-600">Home</Link>
+          <Link to={"/about"} className="text-white px-4 py-2 hover:bg-gray-600">About</Link>
+          {/* Add more links as needed */}
+        </div>
       </div>
     </nav>
   );
--- Phil-ATM-Website/src/Pages/HomePage/HomePage.jsx
+++ Phil-ATM-Website/src/Pages/HomePage/HomePage.jsx
@@ -3,14 +3,32 @@ import Footer from "../../Components/Footer/Footer";
 
 function HomePage() {
   return (
-    <div class="flex container mx-auto max-w-full" id="main-container">
-      <div className="grid w-full sm:h-full">
-        <div>
-          <Header />
-          <h1>Hi I am an h1 inside this DIV</h1>
-          <Footer />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <div className="flex-grow container mx-auto flex items-center justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full h-full max-w-4xl p-4">
+          {/* Left side */}
+          <div className="flex flex-col justify-center">
+            <h1 className="text-3xl font-bold mb-4">Your Trusted ATM Provider for Small Businesses!</h1>
+            <p className="text-md mb-4">
+            Our mission is to empower entrepreneurs like you by providing reliable and efficient ATM solutions tailored to your unique needs.
+            </p>
+            <button className="bg-sky-500 text-white px-6 py-2 w-6/12 rounded hover:bg-sky-600">Contact Us</button>
+          </div>
+
+          {/* Right side */}
+          <div className="flex justify-center">
+            <img
+              src="https://imgur.com/jIo5OHI.png"
+              alt="Your Image"
+              className="max-w-full h-auto rounded-lg shadow-lg"
+            />
+          </div>
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 }
